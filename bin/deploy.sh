acdf70b45b630ea1ffef8435a5b4fd736633fbc4
Hugh Carroll <hughie@carrollonline.co.uk>
2021-06-04 16:21:55 +0100

    Remove bin files from git.

--- bin/deploy.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-JEKYLL_ENV=production bundle exec jekyll b
-git push origin master
-
-config="hugle@gp-uk.net:~/httpdocs/"
-rsync -av --del /home/hughie/sites/hugle.co.uk/hugle/_site/ $config
